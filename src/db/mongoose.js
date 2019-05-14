5c305d2ea72f664b4551889008e209383f4e1ce7
andresweber <andresmweber@gmail.com>
2019-05-14 14:29:25 -0400

    Revised to the zip from the class materials

---
+++ src/db/mongoose.js
@@ -0,0 +1,30 @@
+
+// const MongoClient = require('mongodb').MongoClient;
+// var uri = "mongodb://andresmweber:p5GxnG3AzEgrTV@taskmanager-shard-00-00-lvzkv.mongodb.net:27017,taskmanager-shard-00-01-lvzkv.mongodb.net:27017,taskmanager-shard-00-02-lvzkv.mongodb.net:27017/test?ssl=true&replicaSet=taskmanager-shard-0&authSource=admin&retryWrites=true";
+// MongoClient.connect(uri, {useNewUrlParser: true}, function (err, client) {
+//     console.log(err)
+//     const collection = client.db("test").collection("devices");
+//     client.close();
+// });
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+
+var connection = mongoose.connection
+
+connection.on('connected', () => console.log('connected to db'))
+connection.on('disconnected', () => console.log('disconnected from db'))
+
+connection.on('SIGINT', () => {
+    connection.close(() => {
+        console.log('Lost connection to db due to process termination')
+        process.exit()
+    })
+})
+
+module.exports = connection
