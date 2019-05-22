f9f3f05210a83854305722561621877205e05e6b
andresweber <andresmweber@gmail.com>
2019-05-22 13:23:04 -0400

    Working from home and now can connect to the server, must've been starbucks blocking traffic.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,24 +7,25 @@
 //     client.close();
 // });
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
+console.log('Attempting to connect to server...');
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
 })
 
-var connection = mongoose.connection
+var connection = mongoose.connection;
 
-connection.on('connected', () => console.log('connected to db'))
-connection.on('disconnected', () => console.log('disconnected from db'))
+connection.on('connected', () => console.log('connected to db'));
+connection.on('disconnected', () => console.log('disconnected from db'));
 
 connection.on('SIGINT', () => {
     connection.close(() => {
-        console.log('Lost connection to db due to process termination')
-        process.exit()
-    })
-})
+        console.log('Lost connection to db due to process termination');
+        process.exit();
+    });
+});
 
-module.exports = connection
+module.exports = connection;
