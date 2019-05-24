ada9234dd9f84146d161e3ef73b6a256a85ce257
andresweber <andresmweber@gmail.com>
2019-05-24 15:21:22 -0400

    Basic testing and one test set up.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-const express = require('express');
-require('./db/mongoose');
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
-const indexRouter = require('./routers/index');
-
-const app = express();
+const app = require("./app");
 const port = process.env.PORT;
-
-app.use(express.json());
-app.use(indexRouter);
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
